3354999d446fe9d87587bca7ab713f306960d4eb
anbugit25 <anbunaga82@gmail.com>
2025-03-28 12:08:31 -0400

    Add files via upload

---
+++ SumofTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SumofTwoNumbers {
+
+	public static void main(String[] args) {
+
+		int a=100, b=200;
+		int c=(a+b);
+		System.out.println("Sum of" +a+ " +" +b+ "is :" +c);
+		
+	}
+}
