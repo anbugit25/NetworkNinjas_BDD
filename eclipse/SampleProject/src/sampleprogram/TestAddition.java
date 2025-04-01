3a33ac2d6c6457ebadd94efdf3361073987b4791
[anbugit25] <anbunaga82@gmail.com>
2025-04-01 09:58:43 -0400

    first commit

---
+++ eclipse/SampleProject/src/sampleprogram/TestAddition.java
@@ -0,0 +1,12 @@
+package sampleprogram;
+
+public class TestAddition {
+
+	public static void main(String[] args) {
+		int a=100,b=200;
+		int c=a+b;
+		System.out.println("Sum of "+a+" +"+b+ " is : "+ c);
+
+	}
+
+}
